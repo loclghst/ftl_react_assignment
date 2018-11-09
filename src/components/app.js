8ef480050420327691ef4a56bbd0922347c44392
Manish Kumar Sharma <loclghst@gmail.com>
2018-11-09 13:10:32 +0530

    wire up the components and container to display the app

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,28 @@
-import React, { Component } from 'react';
+import React from "react";
+import { Component } from "react";
+
+import EmployeeList from "../containers/employee-list";
+import EmployeeDetail from '../containers/employee-detail'
+
 
 export default class App extends Component {
+  constructor(props){
+    super(props);
+  }
+
   render() {
     return (
-      <div>Full Throttle Labs react assignment</div>
+      <div className="fluid-container">
+         <div className="col-md-4">
+          <EmployeeList />
+         </div>
+         <div className="col-md-8">
+           <div className="row">
+            <EmployeeDetail />
+           </div>
+
+         </div>
+      </div>
     );
   }
 }

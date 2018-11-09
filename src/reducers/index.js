677dcb2f31cd71a0c16486af20a990960844e997
Manish Kumar Sharma <loclghst@gmail.com>
2018-11-09 09:29:40 +0530

    Wire up the reducers to create the application state

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,11 @@
-import { combineReducers } from 'redux';
+import { combineReducers } from "redux";
+import NamesReducer from "./reducer_names";
+import ActiveEmployee from './reducer_active_employee';
 
 const rootReducer = combineReducers({
+  employees: NamesReducer,
+  activeEmployee: ActiveEmployee
 
 });
-
 export default rootReducer;
+
